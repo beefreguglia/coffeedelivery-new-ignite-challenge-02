abe436488e0a2bb7c6fda054ac04ed4852a9e2fe
bernardo <beefreguglia@gmail.com~>
2022-10-03 13:18:32 -0300

    feature: create global styles and theme

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,18 @@
+export const defaultTheme = {
+  'yellow-100': '#F1E9C9',
+  'yellow-500': '#DBAC2C',
+  'yellow-900': '#C47F17',
+  'purple-100': '#EBE5F9',
+  'purple-500': '#8047F8',
+  'purple-900': '#4B2995',
+  'grey-100': '#FAFAFA',
+  'grey-200': '#F3F2F2',
+  'grey-300': '#EDEDED',
+  'grey-400': '#E6E5E5',
+  'grey-500': '#D7D5D5',
+  'grey-600': '#8D8686',
+  'grey-700': '#574F4D',
+  'grey-800': '#403937',
+  'grey-900': '#272221',
+  white: '#FFFFFF',
+}
